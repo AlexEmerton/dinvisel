260d160dcfa092d4e00533a1c3b0d27c73c5f46c
djuma <ea22zenit@gmail.com>
2021-10-29 21:48:32 +0100

    init

--- bot.py
+++ bot.py
@@ -1,2 +1,46 @@
 from telegram.ext import Updater
-updater = Updater(token='TOKEN', use_context=True)
+from telegram.ext import CommandHandler
+import logging
+import os
+
+TOKEN = '1722312756:AAFi_ipQO6nvqOjf7IREZS7_BgP7GsMneUo'
+PORT = int(os.environ.get('PORT', '8443'))
+APP_NAME = 'https://dinvisel.herokuapp.com'
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def start(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    # log all errors
+    dispatcher.add_error_handler(error)
+
+    # updater.start_polling()
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=PORT,
+                          url_path=TOKEN,
+                          webhook_url=APP_NAME + TOKEN)
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
