029411e0df40ba1e30e76214d99337ad9dd4addd
djuma <ea22zenit@gmail.com>
2021-11-06 19:00:55 +0000

    update video file_id

--- handlers/helpers/consts.py
+++ handlers/helpers/consts.py
@@ -1,5 +1,5 @@
 class Assets:
-    QUICK_VIDEO_ID = "BAACAgQAAxkBAAEOGIhhhIVE3fAudFZ1Ky2uaqVZx6-AWQACZAsAAnnUIVDe2ncFkMxcoSIE"
+    QUICK_VIDEO_ID = "BAACAgQAAxkBAAEOJCphhtDDhyEcCvQJznsQDIKO5Xz8QQACZAsAAnnUIVDe2ncFkMxcoSIE"
     LAUGHING_PIC = "https://assets.heart.co.uk/2018/02/vin-diesel-good-morning-britain-1516363267-herowidev4-0.jpg"
 
 
