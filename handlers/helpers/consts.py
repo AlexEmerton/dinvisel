20bf2c1d98e498e660a7d5e6ced6cf7435c40983
djuma <ea22zenit@gmail.com>
2021-11-06 21:01:34 +0000

    fix lib

--- handlers/helpers/consts.py
+++ handlers/helpers/consts.py
@@ -6,7 +6,7 @@ class VideoCuts:
     cuts = {
         "тигр": "BAACAgQAAxkBAAEOJP1hhuoTeSQMxTLF_hCVaUR8S38hzAACrwkAAqYWMFBY64iaxFwcVCIE",
         "договор": "BAACAgQAAxkBAAEOJQNhhuo1UK6oNR8EkTfrz0Jo2Fdq0wACsAkAAqYWMFDd4yXoS-BpOCIE",
-        "улица": "BAACAgQAAxkBAAEOJQVhhupF6cWRanbjT9d-OOQeG8B9kQACsQkAAqYWMFA-DhCwBnqP_iIE"
+        "улица": "BAACAgQAAxkBAAEOJQVhhupF6cWRanbjT9d-OOQeG8B9kQACsQkAAqYWMFA-DhCwBnqP_iIE",
         "только семья": "BAACAgQAAxkBAAEOJQlhhuqPL3Kdb0z_cg0CSMqb0xr3QQACsgkAAqYWMFADsIszDo2KfSIE",
         "самое важное": "BAACAgQAAxkBAAEOJQthhuq0AAEVatmyXbFB-A19dy3s08oAArMJAAKmFjBQhb58xwGgTmMiBA",
         "слабое место": "BAACAgQAAxkBAAEOJQ1hhurOzeXM18zWgOEagLlz2MRNcgACtAkAAqYWMFDDJBKSDwEJcCIE",
