f4e8255020cc6763ea13f588f8ab6edea312a111
djuma <ea22zenit@gmail.com>
2021-11-07 12:36:14 +0000

    refresh file_ids

--- handlers/helpers/consts.py
+++ handlers/helpers/consts.py
@@ -5,19 +5,19 @@ class Assets:
 class VideoCuts:
     cuts = {
         "тигр": "BAACAgQAAxkBAAEOJ91hh8X9UUJK-dgdBhHgz_0ky-VMCQACNgkAAtwzQFCiAAFAcP2-LIMiBA",
-        "договор": "BAACAgQAAxkBAAEOJQNhhuo1UK6oNR8EkTfrz0Jo2Fdq0wACsAkAAqYWMFDd4yXoS-BpOCIE",
-        "улица": "BAACAgQAAxkBAAEOJQVhhupF6cWRanbjT9d-OOQeG8B9kQACsQkAAqYWMFA-DhCwBnqP_iIE",
-        "только_семья": "BAACAgQAAxkBAAEOJQlhhuqPL3Kdb0z_cg0CSMqb0xr3QQACsgkAAqYWMFADsIszDo2KfSIE",
-        "самое_важное": "BAACAgQAAxkBAAEOJQthhuq0AAEVatmyXbFB-A19dy3s08oAArMJAAKmFjBQhb58xwGgTmMiBA",
-        "слабое_место": "BAACAgQAAxkBAAEOJQ1hhurOzeXM18zWgOEagLlz2MRNcgACtAkAAqYWMFDDJBKSDwEJcCIE",
-        "семья": "BAACAgQAAxkBAAEOJQ9hhurlQbTjh60hQpOXkH7VOpqXxQACtQkAAqYWMFC3ugMjiVi8KiIE",
-        "бегство": "BAACAgQAAxkBAAEOJRFhhur8dPkdYn8ZP_XEbJMXUnDZJAACtgkAAqYWMFCZYwickUVaeCIE",
-        "болтаем": "BAACAgQAAxkBAAEOJRNhhusOG9Yvg-vm8v7XVNrrp6LmrQACtwkAAqYWMFAn4h3OFm9kNSIE",
-        "форма": "BAACAgQAAxkBAAEOJRVhhuseAhFIn05DhdttNQIyPu9QzAACuAkAAqYWMFAYM0zQYlvQ0iIE",
-        "водила": "BAACAgQAAxkBAAEOJRdhhus1Xc2y9-lf_GNEXwShQ57LlAACuQkAAqYWMFDctqejZTUjfCIE",
-        "капот": "BAACAgQAAxkBAAEOJRlhhutFP3ZFnr_A2YGiws65zr_yVgACugkAAqYWMFAVamoeYx_imiIE",
-        "победа": "https://app.box.com/s/ki2ik335sx2rcknlk78684vlx025qwet",
-        "быстро": "BAACAgQAAxkBAAEOJIhhhtmW9D7YndRlxk6iVc6g_D_QNwACmAkAAqYWMFDO8l9QbGJIjyIE"
+        "договор": "BAACAgQAAxkBAAEOJ99hh8bEGW8QwkeEg6KoZigHsHrdzgACNwkAAtwzQFAsmoEnhY7A5SIE",
+        "улица": "BAACAgQAAxkBAAEOJ-Fhh8bjyBZhhxNNQpOTQ2rr5PL1DQACOAkAAtwzQFCzXzvezrVCByIE",
+        "только_семья": "BAACAgQAAxkBAAEOJ-Nhh8cIm-uDDkvc70oxJJolOX_p6AACOQkAAtwzQFB_m7hcvA3sHSIE",
+        "самое_важное": "BAACAgQAAxkBAAEOJ-Vhh8dIkWJEkQn2xXbLcWi6FUgkUAACOgkAAtwzQFAVTai_dJBrfCIE",
+        "слабое_место": "BAACAgQAAxkBAAEOJ-dhh8dcSzrgDR6QpvidSeYhv_zZEwACOwkAAtwzQFDHIvBEKrvrjSIE",
+        "семья": "BAACAgQAAxkBAAEOJ-lhh8dwlpl70WvpnH1o2_b8v3siCQACPAkAAtwzQFBIv1arfRYpLSIE",
+        "бегство": "BAACAgQAAxkBAAEOJ-thh8ejn-95dwy4smHE0Jlj2q8mgwACPQkAAtwzQFAoPzQG-FcSvSIE",
+        "болтаем": "BAACAgQAAxkBAAEOJ-1hh8fE3i7Gi9I1Z1K86Nqrpw6IKAACPgkAAtwzQFBPGo9dt4j2miIE",
+        "форма": "BAACAgQAAxkBAAEOJ-9hh8fZC6-K9C0YSy_Gl9M5TerTrQACPwkAAtwzQFCtEfS6J9aH1iIE",
+        "водила": "BAACAgQAAxkBAAEOJ_Nhh8frJjiRrhM0pBDlRNeWmeyyBQACQAkAAtwzQFC6VrDrVjwSyCIE",
+        "капот": "BAACAgQAAxkBAAEOJ_Vhh8gBzZ0s0mRN-pLY3ek95VSofwACQQkAAtwzQFBCWa90s55B8SIE",
+        "победа": "BAACAgQAAxkBAAEOJ_dhh8gSoorrJzoTgo79JZBVKwwv8AACQgkAAtwzQFCFJPB_rZ7GbyIE",
+        "быстро": "BAACAgQAAxkBAAEOJ_lhh8gunHR9tj-h1M_U_lRFiC-GzgACfAoAAtrFKFCNY_YSlPj5UCIE"
     }
 
 
