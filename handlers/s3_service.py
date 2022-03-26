9696949835ad45fc42fd02088ac84ae015ae2106
djuma <ea22zenit@gmail.com>
2022-03-27 02:02:53 +0500

    refactor s3service variables

--- handlers/s3_service.py
+++ handlers/s3_service.py
@@ -6,5 +6,5 @@ class S3Service:
         self.region_name = app_configs['s3_service']['region_name']
         self.endpoint_name = app_configs['s3_service']['name']
 
-        self.endpoint_url = f"{self.protocol}://{self.endpoint_name}",
-        self.bucket_endpoint_name = f"{self.protocol}://{self.s3_bucket_name}.{self.endpoint_name}"
+        self.endpoint_url = f'{self.protocol}://{self.endpoint_name}'
+        self.bucket_endpoint_name = f'{self.protocol}://{self.s3_bucket_name}.{self.endpoint_name}'
