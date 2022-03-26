89e577c20ec97e423c72f53763554252cac943df
djuma <ea22zenit@gmail.com>
2022-03-27 00:10:09 +0500

    move call_for_rainbow to commands

--- handlers/chat.py
+++ handlers/chat.py
@@ -7,20 +7,7 @@ class Chat:
     def against(self):
         return MessageHandler((Filters.regex(Matchers.SCRIPTED)), self._against)
 
-    def call_for_rainbow(self):
-        return MessageHandler(
-            (Filters.entity('mention') & Filters.regex(Matchers.GO)),
-            self._call_for_rainbow)
-
     @staticmethod
     def _against(update, context):
         context.bot.send_message(
             chat_id=update.effective_chat.id, text="против Азика")
-
-    @staticmethod
-    def _call_for_rainbow(update, context):
-        message = context.bot.send_message(
-            chat_id=update.effective_chat.id, text="сегодня го?")
-
-        context.bot.pin_chat_message(
-            update.effective_chat.id, message['message_id'])
--- handlers/command.py
+++ handlers/command.py
@@ -10,10 +10,21 @@ class Command:
     def get_stats_for_name(self):
         return CommandHandler('stats', self._get_stats_for_name)
 
+    def call_for_rainbow(self):
+        return CommandHandler('go', self._call_for_rainbow)
+
     @ staticmethod
     def _start(update, context):
         context.bot.send_message(
             chat_id=update.effective_chat.id, text="yo")
+
+    @staticmethod
+    def _call_for_rainbow(update, context):
+        message = context.bot.send_message(
+            chat_id=update.effective_chat.id, text="сегодня го?")
+
+        context.bot.pin_chat_message(
+            update.effective_chat.id, message['message_id'])
 
     @ staticmethod
     def _get_stats_for_name(update, context):
