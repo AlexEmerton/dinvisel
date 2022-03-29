ff28516779b28735ac8fa2322c4ffda1017c025a
djuma <ea22zenit@gmail.com>
2022-03-30 00:14:21 +0500

    try join al args

--- handlers/voice.py
+++ handlers/voice.py
@@ -9,7 +9,7 @@ class Voice:
         return CommandHandler('voice', self._send_tts)
 
     def _send_tts(self, update, context):
-        tts_text = context.args[0]
+        tts_text = ' '.join(context.args)
 
         tts = gTTS(text=tts_text, lang=self.DEFAULT_LANG)
         tts.save('tts.mp3')
